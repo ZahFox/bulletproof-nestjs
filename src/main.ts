3b3751b9dbc7ae2200f30c686ada6e6f3eeb824c
William McCullough <ZahFoxTales@gmail.com>
2020-03-21 23:07:46 -0500

    feat: added winston as the application logger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from './loaders';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    const app: INestApplication = await NestFactory.create(AppModule);
+    await app.listen(3000);
+    Logger.info('app listening on port 3000')
+  } catch (err) {
+    Logger.error('failed to boostrap the application')
+    Logger.error(err)
+  }
 }
 
 bootstrap();
